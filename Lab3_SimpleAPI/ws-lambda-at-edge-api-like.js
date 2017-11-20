a51abf7a35167b78146c6715deb6591c28d45064
Alexander Korobeynikov <akorob@amazon.com>
2017-11-20 15:43:04 -0800

    Add images for Lab3

--- Lab3_SimpleAPI/ws-lambda-at-edge-api-like.js
+++ Lab3_SimpleAPI/ws-lambda-at-edge-api-like.js
@@ -4,21 +4,21 @@ const QS = require('querystring');
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB({apiVersion: '2012-10-08', region: 'us-east-1'});
 
-const ddbTableName = 'AlienCards-ae4589f0';
+const ddbTableName = FIXME; // Copy DynamoDB table name here, for example, 'AlienCards-1201c610'
 
 exports.handler = (event, context, callback) => {
-    console.log('Event:', JSON.stringify(event, null, 2));
-    console.log('Context:', JSON.stringify(context, null, 2));
+    console.log('Event: ', JSON.stringify(event, null, 2));
+    console.log('Context: ', JSON.stringify(context, null, 2));
     const request = event.Records[0].cf.request;
     const params = QS.parse(request.querystring);
     
     if (request.method != 'POST')
-        return generateResponse(callback,
-            { status: '400', body: { error: "Bad HTTP verb, expected POST" } });
+        return callback(null, getResponse(
+            { status: '400', body: { error: "Bad HTTP verb, expected POST" } }));
 
     if (!params.id)
-        return generateResponse(callback,
-            { status: '400', body: { error: "Couldn't parse id parameter" } });
+        return callback(null, getResponse(
+            { status: '400', body: { error: "Couldn't parse id parameter" } }));
 
     ddb.updateItem({ 
         TableName: ddbTableName,
@@ -27,31 +27,25 @@ exports.handler = (event, context, callback) => {
         ExpressionAttributeValues: { ":one": {"N": "1"} },
         Key: { CardId: { S: params.id } }
     }, (err, data) => {
-        console.log('err:' + JSON.stringify(err));
-        console.log('data:' + JSON.stringify(data));
+        console.log('err: ' + JSON.stringify(err));
+        console.log('data: ' + JSON.stringify(data));
         if (err)
-            return generateResponse(callback, 
-                { status: '500', body: err });
+            return callback(null, getResponse({ status: '500', body: err }));
         else
-            return generateResponse(callback, 
-                { status: '200', body: flattenItem(data) });
+            return callback(null, getResponse({ status: '200', body: flattenItem(data) }));
     });
 };
 
-function generateResponse(callback, resp) {
-    const desc = {
-        '200': 'OK',
-        '400': 'Bad Request',
-        '500': 'Internal Server Error'
-    };
-    callback(null, {
+function getResponse(resp) {
+    const desc = { '200': 'OK', '400': 'Bad Request', '500': 'Internal Server Error' };
+    return {
         status: resp.status,
         statusDescription: desc[resp.status],
         headers: addSecurityHeaders({
             'content-type': [{ key: 'Content-Type',  value: 'application/json' }]
         }),
         body: JSON.stringify(resp.body, null, 2)
-    });
+    };
 }
 
 function flattenItem(item) {
